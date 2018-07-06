99f4373d803ec005304f800358fe9a2c5d02e793
ronald vilorio <soccer23415@gmail.com>
2018-07-06 19:01:10 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+
+  end
+  def create
+    @game = Game.create(state: ["X", "", "", "", "", "", "", "", ""])
+    render json: @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game.id
+  end
+
+  def update
+  end
 
 end
